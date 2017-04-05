8e94e5db687be6a19986379723e5dcca38cb8fa8
Koykis <koykis@gmail.com>
2017-04-05 13:57:37 +0300

    Recipe tutorial
    Completed the basic application structure.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test.', 'http://3.bp.blogspot.com/-vu9YPd9Cb6Q/UdpvCFC_gdI/AAAAAAAAZSM/Jf7Kv2W0SQ8/s1600/recipe-book-design-5c.jpg'),
     new Recipe('A Test Recipe', 'This is simply a test.', 'http://3.bp.blogspot.com/-vu9YPd9Cb6Q/UdpvCFC_gdI/AAAAAAAAZSM/Jf7Kv2W0SQ8/s1600/recipe-book-design-5c.jpg')
   ];
 
